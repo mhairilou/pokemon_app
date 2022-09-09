10a1cd7830907ebe8ac08c21f1cc7ab80ba5e11a
Mhairi MacDonald <mhairilouyoga@gmail.com>
2022-09-09 21:59:24 +0100

    added types

--- src/components/PokemonCardComponent.jsx
+++ src/components/PokemonCardComponent.jsx
@@ -12,23 +12,28 @@ const PokemonCard = ({ selectedPokemonUrl }) => {
                     console.log("do we have data", pokemonData)
                     setPokemonDetails(pokemonData)
                 })
+
         }
     }, [selectedPokemonUrl]);
+
+    //pokemonTypes is an array of type values found by looping through pokemonDetails.types
+    //and for each element in .types going to the type.name
+
 
 
     if (pokemonDetails === null)
         return null
 
-    console.log(pokemonDetails.sprites.front_default)
-    console.log("this is the alt text", pokemonDetails.name)
-
     return (
 
-        <>
-            <h5> I am a pokemon card! I will render a sprite and some stats from here: {selectedPokemonUrl}</h5>
+        <div>
+            <h3> {pokemonDetails.name} </h3>
             <img src={pokemonDetails.sprites.front_default} alt={`Image of ${pokemonDetails.name}`}></img>
+            <h4>Type(s):</h4>
+            <p>{pokemonDetails.types[0].type.name}</p>
+            <p>{pokemonDetails.types[1]?.type.name}</p>
 
-        </>
+        </div>
     )
 
 
