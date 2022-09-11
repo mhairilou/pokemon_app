28b237c8034afa36f66bd96a4b6f3da98348e86e
Mhairi MacDonald <mhairilouyoga@gmail.com>
2022-09-11 22:19:04 +0100

    more css

--- src/components/PokemonTeam.jsx
+++ src/components/PokemonTeam.jsx
@@ -3,23 +3,24 @@ import "./PokemonTeam.css"
 
 //This component's job is to pass props to the Cards for each Pokemon on the Team.
 
-const PokemonTeam = ({ currentTeamList, removePokemonFromTeam}) => {
+const PokemonTeam = ({ currentTeamList, removePokemonFromTeam }) => {
 
-    if(currentTeamList === undefined)
-    return null
+    if (currentTeamList === undefined)
+        return null
 
     return (
-        <div className="team">
+        <div>
             {currentTeamList.length !== 0 && <h2>Your team:</h2>}
-            {currentTeamList.map(({ id, url, nickname }) => {
-                console.log(id)
-                return (
-                    <PokemonCard key={id} id={id} pokemonUrl={url} nickname={nickname} currentTeamList={currentTeamList} remove={removePokemonFromTeam}/>
-                )
+            <div className="team">
+                {currentTeamList.map(({ id, url, nickname }) => {
+                    console.log(id)
+                    return (
+                        <PokemonCard key={id} id={id} pokemonUrl={url} nickname={nickname} currentTeamList={currentTeamList} remove={removePokemonFromTeam} />
+                    )
 
-            })}
-            
+                })}
 
+            </div>
         </div>
     )
 };
